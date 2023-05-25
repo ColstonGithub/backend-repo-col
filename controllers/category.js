61a517c74410190c1f57b317c3943436f065aedb
Preetam Kumar <preetamkumar3232@gmail.com>
2023-05-25 13:49:48 +0530

    added testing

--- controllers/category.js
+++ controllers/category.js
@@ -170,7 +170,7 @@ exports.deleteCategories = async (req, res) => {
 
     if (response) {
       let newBannerImage = response?.categoryImage.replace(
-        "http://localhost:5000/public/",
+        "http://64.227.150.49:5000/public/",
         ""
       );
       const imagepath1 = path.join(__dirname, "../uploads", newBannerImage);
