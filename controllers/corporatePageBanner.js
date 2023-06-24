11b6e2086556d55e75504222ba76ef009121066f
Preetam Kumar <preetamkumar3232@gmail.com>
2023-06-24 18:45:45 +0530

    explore cat

--- controllers/corporatePageBanner.js
+++ controllers/corporatePageBanner.js
@@ -3,13 +3,37 @@ const shortid = require("shortid");
 const slugify = require("slugify");
 const path = require("path");
 const fs = require("fs");
-exports.createCorporatePageBanner = (req, res) => {
-  try {
-    const { title, bannerImageAltText, bannerImageTextAltText } = req.body;
 
-    const bannerImage = req.files["bannerImage"]
-      ? process.env.API + "/public/" + req.files["bannerImage"][0].filename
-      : undefined;
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  endpoint: new AWS.Endpoint("https://sgp1.digitaloceanspaces.com"), // Replace with your DigitalOcean Spaces endpoint
+  accessKeyId: "DO00DRWTB9KLHRDV4HCB", // Replace with your DigitalOcean Spaces access key ID
+  secretAccessKey: "W2Ar0764cy4Y7rsWCecsoZxOZ3mJTJoqxWBo+uppV/c", // Replace with your DigitalOcean Spaces secret access key
+});
+
+exports.createCorporatePageBanner = async (req, res) => {
+  try {
+    const { title, bannerImageAltText } = req.body;
+
+    let bannerImage = "";
+
+    if (req.file) {
+      const fileContent = req.file.buffer;
+      const filename = shortid.generate() + "-" + req.file.originalname;
+      const uploadParams = {
+        Bucket: "colston-images", // Replace with your DigitalOcean Spaces bucket name
+        Key: filename,
+        Body: fileContent,
+        ACL: "public-read",
+      };
+
+      // Upload the file to DigitalOcean Spaces
+      const uploadedFile = await s3.upload(uploadParams).promise();
+
+      // Set the image URL in the bannerImage variable
+      bannerImage = uploadedFile.Location;
+    }
 
     const PageBanner = new CorporatePageBanner({
       title,
@@ -20,12 +44,7 @@ exports.createCorporatePageBanner = (req, res) => {
     if (bannerImageAltText != undefined) {
       PageBanner.bannerImageAltText = bannerImageAltText;
     }
-    if (bannerImageTextAltText != undefined) {
-      PageBanner.bannerImageTextAltText = bannerImageTextAltText;
-    }
-    if (bannerImageText != undefined) {
-      PageBanner.bannerImageText = bannerImageText;
-    }
+
     if (bannerImage != undefined) {
       PageBanner.bannerImage = bannerImage;
     }
