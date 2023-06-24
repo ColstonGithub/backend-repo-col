c3d7a216892b9fe2606ac963a5768dddb436bff0
Preetam Kumar <preetamkumar3232@gmail.com>
2023-06-24 17:52:48 +0530

    added career

--- controllers/career.js
+++ controllers/career.js
@@ -3,13 +3,37 @@ const shortid = require("shortid");
 const slugify = require("slugify");
 const path = require("path");
 const fs = require("fs");
-exports.createCareer = (req, res) => {
+
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  endpoint: new AWS.Endpoint("https://sgp1.digitaloceanspaces.com"), // Replace with your DigitalOcean Spaces endpoint
+  accessKeyId: "DO00DRWTB9KLHRDV4HCB", // Replace with your DigitalOcean Spaces access key ID
+  secretAccessKey: "W2Ar0764cy4Y7rsWCecsoZxOZ3mJTJoqxWBo+uppV/c", // Replace with your DigitalOcean Spaces secret access key
+});
+
+exports.createCareer = async (req, res) => {
   try {
     const { name, email, subject, mobileNo, message } = req.body;
 
-    const pdf = req.file
-      ? process.env.API + "/public/" + req.file.filename
-      : undefined;
+    let pdf;
+
+    if (req.file) {
+      const fileContent = req.file.buffer;
+      const filename = shortid.generate() + "-" + req.file.originalname;
+      const uploadParams = {
+        Bucket: "colston-images", // Replace with your DigitalOcean Spaces bucket name
+        Key: filename,
+        Body: fileContent,
+        ACL: "public-read",
+      };
+
+      // Upload the file to DigitalOcean Spaces
+      const uploadedFile = await s3.upload(uploadParams).promise();
+
+      // Set the image URL in the bannerImage variable
+      pdf = uploadedFile.Location;
+    }
 
     const careerData = new Career({
       name,
