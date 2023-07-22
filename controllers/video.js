4a7c715946a4d1b1bb72de6a24d795375bc36e87
Preetam Kumar <preetamkumar3232@gmail.com>
2023-07-22 16:33:47 +0530

    added

--- controllers/video.js
+++ controllers/video.js
@@ -21,7 +21,7 @@ const axios = require("axios");
 
 exports.getVideos = async (req, res) => {
   const CHANNEL_ID = "UCetQjHCqSAqlPxY8VJVpnOg"; // Replace with the desired channel ID
-  const API_KEY = "AIzaSyDkTYtVt6IASK_wg71A3gF1cpdMJrLrwjg"; // Replace with your YouTube API key
+  const API_KEY = "AIzaSyDsZgNK6bxs4BVg7j2iNRHMCcbC04FaF4I"; // Replace with your YouTube API key
   const pageToken = req.query.pageToken || ""; // Get the pageToken from the query params
 
   const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=50&pageToken=${pageToken}`;
